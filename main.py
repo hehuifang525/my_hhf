ba40e51af9199f1cc4bfb04d45531a0bfdca6571
hhf <565997450@qq.com>
2022-04-06 18:22:16 +0800

    提交修改的代码

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 """
 from test_sjqd import sjqd
 
-if __name__ == '__main__':
-    sjqd("data/test01.xlsx")
+# if __name__ == '__main__':
+#     sjqd("data/test01.xlsx")
 
+import pytest
+import os
+import allure
+if __name__ == '__main__':
+    pytest.main(["-vs", "test_case/test_case01.py", '--alluredir','./report_temp', '--clean-alluredir'])
+    # os.system('allure serve C:\PycharmProjects\key_word\\report_temp')  # 立即打开报告（程序会持续执行）
+    os.system('allure serve ./report_temp')  # 立即打开报告（程序会持续执行）
+    # os.system('allure generate ./report_temp/ -o ./report_allure --clean') # 生成报告到report_allure文件中
--- test_case/test_case01.py
+++ test_case/test_case01.py
@@ -1,3 +1,6 @@
 """
     测试用例书写，主要用于验证其他的封装
 """
+from test_sjqd import sjqd
+def test_001():
+    sjqd("data/test01.xlsx")
